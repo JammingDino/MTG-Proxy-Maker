881f59aac55f7b6f64ad079c13292ca4882c4cd8
JammingDino <levipronkjones1@gmail.com>
2025-03-25 21:22:00 +1300

    Reformated and got card gathering from scryfall to a comfertably working state

--- main.py
+++ main.py
@@ -1,25 +1,7 @@
 import eel
-from requests import get
-from shutil import copyfileobj
 import scrython
 import scrython.cards
-import os
-import glob
-
-card_image_dir = 'web/card_images'
-
-# Ensure the directory exists
-os.makedirs(card_image_dir, exist_ok=True)
-
-def cleanup_images():
-    """ Remove all .jpg files from the card images directory. """
-    print("Cleaning up card images...")
-    for file_path in glob.glob(os.path.join(card_image_dir, "*.jpg")):
-        try:
-            os.remove(file_path)
-            print(f"Deleted: {file_path}")
-        except Exception as e:
-            print(f"Failed to delete {file_path}: {e}")
+import json
 
 eel.init("web")
 
@@ -29,30 +11,25 @@ def js_print(string) -> None:
 
 @eel.expose
 def random_card_image() -> str:
-    print("Calling the function")
     card = scrython.cards.Random()
-
-    img_file = 'web/card_images/' + card.name() + ".jpg"
-
-    with open(img_file, 'wb') as out_file:
-        copyfileobj(get(card.image_uris()['normal'], stream = True).raw, out_file)
-    
-    return img_file[3:]
+    return card.image_uris()['normal']
 
 @eel.expose
-def find_specific_card(card : str = "island") -> str:
+def load_specific_card(card : str = "island") -> str:
     try:
-        card = scrython.cards.Named(fuzzy=card)
+        card_information = scrython.cards.Named(fuzzy=card).scryfallJson
+        print(json.dumps(card_information, indent=4))
 
-        img_file = 'web/card_images/' + card.name() + ".jpg"
-        with open(img_file, 'wb') as out_file:
-            copyfileobj(get(card.image_uris()['normal'], stream = True).raw, out_file)
-        
-        return img_file[3:]
-    except:    
+        eel.change_class_text("card-text-card-name", card_information["name"])
+        eel.change_class_image("card-image", card_information['image_uris']['normal'])
+        eel.change_class_text("card-text-mana-cost", card_information["mana_cost"])
+        eel.change_class_text("card-text-type-line", card_information["type_line"])
+        eel.change_class_text("card-text-artist", card_information["artist"])
+        eel.change_class_text("card-text-oracle", card_information["oracle_text"])
+
+        return card_information
+    except Exception as e:    
+        print(e)
         return False
 
-try:
-    eel.start("index.html", size=(680, 800))
-finally:
-    cleanup_images()
+eel.start("index.html")
--- web/script.js
+++ web/script.js
@@ -1,65 +1,96 @@
-eel.expose
-async function addImage(imagePath) {
-    eel.js_print("Button clicked, attempting to call Python function...");
+eel.expose(change_class_text)
+
+function change_class_text(element_class, text) {
+    let element = document.getElementsByClassName(element_class)[0];
+
+    // Clear all child elements
+    element.innerHTML = "";  
+
+    if (text.includes("\n")) {
+        let lines = text.split("\n");
+
+        lines.forEach(line => {
+            let p = document.createElement("p");
+            p.textContent = line;
+            element.appendChild(p);
+        });
+    } else {
+        let p = document.createElement("p");
+        p.textContent = text;
+        element.appendChild(p);
+    }
+}
+
+
+eel.expose(change_class_image)
+function change_class_image(element_class, src) {
+    let element = document.getElementsByClassName(element_class)[0];
+    element.src = src
+    element.alt = src
+}
+
+async function addCard(cardInformation) {
 
     try {
         // Specify the image path (change this to your actual image) 
-        eel.js_print(imagePath);
+        eel.js_print(cardInformation);
 
         // Create a new div element for the frame
-        let frame = document.createElement("div");
-        frame.classList.add("image-frame");
+        let frame        = document.getElementsByClassName("card-image-frame")[0];
+        let img          = document.getElementsByClassName("card-image")[0];
+        let name         = document.getElementsByClassName("card-text-card-name")[0];
+        let mana_value   = document.getElementsByClassName("card-text-mana-cost")[0];
+        let type_line    = document.getElementsByClassName("card-text-type-line")[0];
 
-        // Create the img element
-        let img = document.createElement("img");
-        img.src = imagePath;
-        img.alt = imagePath;
+        let stats        = document.getElementsByClassName("card-text-stats")[0];
+        let artist       = document.getElementsByClassName("card-text-artist")[0];
 
-        // Add the mousemove event to the frame for interactive tilt
-        frame.addEventListener("mousemove", function(event) {
-            const bounds = frame.getBoundingClientRect(); // Get the size and position of the frame
-            const offsetX = event.clientX - bounds.left; // Mouse X position relative to the frame
-            const offsetY = event.clientY - bounds.top;  // Mouse Y position relative to the frame
+        img.src                = cardInformation.image;
+        name.textContent       = cardInformation.name;
+        mana_value.textContent = cardInformation.mana_cost;
+        type_line.textContent  = cardInformation.type_line
+        stats.textContent      = cardInformation.stats;
+        artist.textContent     = cardInformation.artist;
 
-            // Normalize the mouse position to be between -1 and 1 (relative to frame size)
-            const xRotation = ((offsetY / bounds.height) - 0.5) * 10;  // Tilt on the X axis (subtle)
-            const yRotation = ((offsetX / bounds.width) - 0.5) * -10; // Tilt on the Y axis (subtle)
+        // // Add the mousemove event to the frame for interactive tilt
+        // frame.addEventListener("mousemove", function(event) {
+        //     const bounds = frame.getBoundingClientRect(); // Get the size and position of the frame
+        //     const offsetX = event.clientX - bounds.left; // Mouse X position relative to the frame
+        //     const offsetY = event.clientY - bounds.top;  // Mouse Y position relative to the frame
 
-            // Apply the tilt to the image inside the frame
-            img.style.transform = `rotateX(${xRotation}deg) rotateY(${yRotation}deg)`;
-        });
+        //     // Normalize the mouse position to be between -1 and 1 (relative to frame size)
+        //     const xRotation = ((offsetY / bounds.height) - 0.5) * 10;  // Tilt on the X axis (subtle)
+        //     const yRotation = ((offsetX / bounds.width) - 0.5) * -10; // Tilt on the Y axis (subtle)
 
-        // Add mouseout event to reset the tilt when the mouse leaves the frame
-        frame.addEventListener("mouseout", function() {
-            img.style.transform = "rotateX(0deg) rotateY(0deg)";  // Reset tilt
-        });
+        //     // Apply the tilt to the image inside the frame
+        //     img.style.transform = `rotateX(${xRotation}deg) rotateY(${yRotation}deg)`;
+        // });
 
-        // Append the image to the frame
-        frame.appendChild(img);
+        // frame.addEventListener("mouseout", function() {
+        //     img.style.transform = "rotateX(0deg) rotateY(0deg)";
+        // });
 
-        // Append the frame to the container
-        document.getElementById("imageContainer").appendChild(frame);
     } catch (error) {
         eel.js_print(error);
     }
 }
 
-eel.expose
 async function sendText() {
-    let text = document.getElementById("textBox").value;
+    let text = document.getElementById("text-box").value;
     if (text.trim() === "") {
         showPopupMessage("The card you entered does not exist!");
         return;
     } else {
-        let imagePath = await eel.find_specific_card(text)();
+        eel.load_specific_card(text);
+        // let cardInformation = await eel.find_specific_card(text)();
 
-        if (imagePath === false) {
-            showPopupMessage("The card you entered does not exist!");
-            return
-        }
+        // if (cardInformation === false) {
+        //     showPopupMessage("The card you entered does not exist!");
+        //     return
+        // }
         
-        eel.js_print(imagePath);
-        addImage(imagePath);
+        // eel.js_print(cardInformation);
+        // addCard(cardInformation);
     }
 }
 
